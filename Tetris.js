c96a08bc1568644ac9497a3d9540f97f8dc4a6ee
PUNIT SINGH BISHT <114064992+PSBVISION@users.noreply.github.com>
2024-02-06 21:50:50 +0530

    initial commit

---
+++ Tetris.js
@@ -0,0 +1,65 @@
+class Tetris{
+    constructor(imageX, imageY, template) {
+        this.imageY = imageY;
+        this.imageX = imageX;
+        this.template = template;
+    }
+
+    checkDown(){}
+
+    checkLeft(){}
+
+    checkRight(){}
+
+    moveRigtht(){}
+
+    moveLeft(){}
+
+    moveDown(){}
+
+    changeRotation(){}
+}
+
+const imageSquareSize = 24;
+const size = 40;
+const framePerSecond = 24;
+const gameSpeed = 5;
+const canvas = document.getElementById('canvas');
+const image = document.getElementById('image');
+const ctx = canvas.getContext('2d');
+const squareCountX = canvas.width / size;
+const squareCountY = canvas.height / size;
+
+const shapes = [
+    new Tetris(0,120, [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0], 
+    ],
+    new Tetris(0,96, [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0], 
+    ],
+    new Tetris(0,72, [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1], 
+    ],
+    new Tetris(0,48, [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0], 
+    ],
+    new Tetris(0,24, [
+        [0, 1, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0], 
+    ],
+    new Tetris(0,0, [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0], 
+    ],
+    ),
+];
